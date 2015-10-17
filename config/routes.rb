3e805f0c4f000f06c337122e77aa86f06741ddb5
Valeskyta <valeska.arquitecto@gmail.com>
2015-10-16 21:36:28 -0300

    create comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :posts
+  get 'comments/create'
+
+  resources :posts do
+    resources :comments, only: [:create]
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -55,3 +59,4 @@ Rails.application.routes.draw do
   #     resources :products
   #   end
 end
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Post.destroy_all
+Comment.destroy_all
+
+posts = []
+(1..20).each do |n|
+  posts << Post.create(title: "post nº #{n}", content: "contenido del postnumero #{n}")
+end
+
+comments = []
+(1..50).each do |n|
+  comments << Comment.create(comment: "comentario nº#{n}", post: posts[rand(1..(posts.length))])
+end
